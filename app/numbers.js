8e779d02dc40e64e7ba380a2227e3aa734c85a32
MeesterMeetch <mwhunt3@gmail.com>
2015-06-28 22:59:27 -0400

    more answers added.

--- app/numbers.js
+++ app/numbers.js
@@ -2,11 +2,13 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.numbersAnswers = {
   valueAtBit: function(num, bit) {
-
+    var binary = num.toString(2);
+    var binaryLength = binary.length;
+    return Number(binary.charAt(binaryLength - bit));
   },
 
   base10: function(str) {
-
+    return parseInt(str, 2);
   },
 
   convertToBinary: function(num) {
@@ -14,6 +16,7 @@ exports.numbersAnswers = {
   },
 
   multiply: function(a, b) {
-
-  }
+      var result = a * b
+      return parseFloat(result.toPrecision(15));
+    }
 };
